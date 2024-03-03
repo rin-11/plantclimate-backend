993f56c9e4608c172ea29f25d80a7f0007522056
rin-11 <erinkates11@gmail.com>
2024-03-03 18:50:40 -0500

    search working

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.use(express.json());
 app.use(cors());
 
 // ROUTES
+// Trefle API search route
+app.get('/api/plants/search', async (req, res) => {
+    const query = req.query.q; // Get the search query from request query parameters
+    const trefleToken = process.env.TREFLE_TOKEN; // Your Trefle API token stored in .env
+  
+    try {
+      const response = await fetch(`https://trefle.io/api/v1/plants/search?token=${trefleToken}&q=${query}`);
+      const json = await response.json();
+      res.json(json);
+    } catch (error) {
+      console.error('Error fetching data from Trefle API:', error);
+      res.status(500).json({ error: 'Failed to fetch data from Trefle API' });
+    }
+  });
+  
 // Route to get all plants
 app.get('/api/plants', async (req, res) => {
     const trefleToken = process.env.TREFLE_TOKEN;
